d6b9f57be8d1f185ca548131891a94fa8e63d720
RameshwarSuryawanshi <71263128+RameshwarSuryawanshi@users.noreply.github.com>
2023-06-01 16:51:19 +0530

    Update server.tf

--- server.tf
+++ server.tf
@@ -16,9 +16,9 @@ resource "aws_instance" "myawsserver" {
   key_name = "rameshwar-usw2-key1"
 
   tags = {
-    Name = "Raman-DevOps-batch-server"
+    Name = "Ram-DevOps-batch-server"
     env = "Production"
-    owner = "Raman Khanna"
+    owner = "Ram"
   }
   provisioner "local-exec" {
     command = "echo The servers IP address is ${self.public_ip} && echo ${self.public_ip} > /tmp/inv"
